c6d1d5cd881aa12287051b7c0e884884b638a5b1
fadrian06 <franyeradriansanchez@gmail.com>
2023-12-07 20:24:50 -0400

    Add cards colors

--- src/consts.ts
+++ src/consts.ts
@@ -1,25 +1,25 @@
 import type { CSSProperties } from 'react'
 
-// export const colors: CSSProperties['color'][] = [
-//   'darkblue',
-//   'darkcyan',
-//   'darkgoldenrod',
-//   'darkgray',
-//   'darkgreen',
-//   'darkkhaki',
-//   'darkmagenta',
-//   'darkolivegreen',
-//   'darkorange',
-//   'darkorchid',
-//   'darkred',
-//   'darksalmon',
-//   'darkseagreen',
-//   'darkslateblue',
-//   'darkslategray',
-//   'darkturquoise',
-//   'darkviolet'
-// ]
+export const colors: CSSProperties['color'][] = [
+  'darkblue',
+  'darkcyan',
+  'darkgoldenrod',
+  'darkgray',
+  'darkgreen',
+  'darkkhaki',
+  'darkmagenta',
+  'darkolivegreen',
+  'darkorange',
+  'darkorchid',
+  'darkred',
+  'darksalmon',
+  'darkseagreen',
+  'darkslateblue',
+  'darkslategray',
+  'darkturquoise',
+  'darkviolet'
+]
 
-export const colors: CSSProperties['color'][] = ['#333']
+// export const colors: CSSProperties['color'][] = ['#333']
 
 export const bingoSymbol = '★'
